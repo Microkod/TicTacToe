704a8c9164640c62de8c5379281f01457362b081
Oskar Woźniak <oskar.tomasz.wozniak@gmail.com>
2022-04-13 14:28:59 +0200

    added frame

---
+++ src/main/java/TicTacToe.java
@@ -0,0 +1,61 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+public class TicTacToe implements ActionListener {
+
+    Random random = new Random();
+    JFrame frame = new JFrame();
+    JPanel titlePanel = new JPanel();
+    JPanel buttonPanel = new JPanel();
+    JLabel textField = new JLabel();
+    JButton[] buttons = new JButton[9];
+    boolean player1Turn;
+
+    TicTacToe() {
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 800);
+        frame.getContentPane().setBackground(new Color(50, 50, 50));
+        frame.setLayout(new BorderLayout());
+        frame.setVisible(true);
+
+        textField.setBackground(new Color(25, 25, 25));
+        textField.setForeground(new Color(25, 255, 0));
+        textField.setFont(new Font("Arial", Font.BOLD, 75));
+        textField.setHorizontalAlignment(JLabel.CENTER);
+        textField.setText("TicTacToe");
+        textField.setOpaque(true);
+
+        titlePanel.setLayout(new BorderLayout());
+        titlePanel.setBounds(0,0,800,100);
+
+        titlePanel.add(textField);
+        frame.add(titlePanel);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    public void firstTurn() {
+
+    }
+
+    public void check() {
+
+    }
+
+    public void xWins(int a, int b, int c) {
+
+    }
+
+    public void oWins(int a, int b, int c) {
+
+    }
+
+}
